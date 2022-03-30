a2d425c0d28a48a8eb814125a2dfa4c20180d801
shivaan010 <99326549+shivaan010@users.noreply.github.com>
2022-03-30 13:37:51 +0530

    Update main.py
    remove email sending

--- main.py
+++ main.py
@@ -2,13 +2,8 @@ from datetime import *
 import wikipedia
 import pywhatkit
 import pyttsx3
-import smtplib
 
 speaker = pyttsx3.init()
-
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.starttls()
-server.login('ishaans142@gmail.com','programming')
 
 try:
     while True:
@@ -40,22 +35,6 @@ try:
             song = command.replace('play', '')
             print('playing....')
             pywhatkit.playonyt(song)
-
-        elif 'table' in command:
-            table = int(input('Which table too tell: '))
-            i = 1
-            while i <= 10:
-                b = table, 'times', i, '=', table*i
-                speaker.say(b)
-                speaker.runAndWait()
-                i = i+1
-
-        elif 'email' in command:
-            sendto = input('who to send email:')
-            text = input('Enter text: ')
-
-            server.sendmail('ishaans@gmail', sendto, text)
-            print('send successfully!')
 
 except:
     print('bip bip bip!!')
